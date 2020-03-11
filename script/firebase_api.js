ff9a9e39a784399bdb07589f819b1c7fc266e3ba
Timothy Tan <tim.tan5145@gmail.com>
2020-03-11 11:48:04 -0700

    Added firebase

---
+++ script/firebase_api.js
@@ -0,0 +1,21 @@
+/*
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.10.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+*/
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDaKyXsddEuxKgrfD47zktGPowMDmA5L-c",
+    authDomain: "projecthandwash-34333.firebaseapp.com",
+    databaseURL: "https://projecthandwash-34333.firebaseio.com",
+    projectId: "projecthandwash-34333",
+    storageBucket: "projecthandwash-34333.appspot.com",
+    messagingSenderId: "269852739877",
+    appId: "1:269852739877:web:09a3a7b7fcaf7ffdc572cc"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
