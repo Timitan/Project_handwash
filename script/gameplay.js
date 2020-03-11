372778e82b20298a623bb20970f796e9c6c8e317
Joban <joban.nijjer145@gmail.com>
2020-03-11 12:33:28 -0700

    Changes to Store list

--- script/gameplay.js
+++ script/gameplay.js
@@ -1,20 +1,19 @@
-function toggleShop(){
+function toggleShop() {
     let shopMenu = document.getElementById("mySideNav")
     let height = shopMenu.style.height;
     let openHeight = "45vh";
     let closedHeight = "0vh";
 
-    if(height == closedHeight  || height == ""){    
+    if (height == closedHeight || height == "") {
         shopMenu.style.height = openHeight;
     } else {
         shopMenu.style.height = closedHeight;
     }
 }
 
-function incrementScore(){
+function incrementScore() {
     let scoreDisplay = document.getElementById("money-display");
     let money = scoreDisplay.innerHTML.slice(1);
     money++;
     scoreDisplay.innerHTML = "$ " + money;
-
 }
