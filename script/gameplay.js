466515665b5e20bab739c2e76444a7ea72fd9664
Timothy Tan <tim.tan5145@gmail.com>
2020-03-13 16:12:28 -0700

    Moved shop menu and restyled it, added healthbar that decreases over time and regains with every hand click

--- script/gameplay.js
+++ script/gameplay.js
@@ -1,13 +1,18 @@
+// Global Variables
+let healthValue = 100;
+let healthDecreaseRate = 0.015;
+let clickGainHealthRate = 0.85;
+
 function toggleShop() {
     let shopMenu = document.getElementById("mySideNav")
-    let height = shopMenu.style.height;
-    let openHeight = "45vh";
-    let closedHeight = "0vh";
+    let height = shopMenu.style.width;
+    let openValue = "45vh";
+    let closedValue = "0vh";
 
-    if (height == closedHeight || height == "") {
-        shopMenu.style.height = openHeight;
+    if (height == closedValue || height == "") {
+        shopMenu.style.width = openValue;
     } else {
-        shopMenu.style.height = closedHeight;
+        shopMenu.style.width = closedValue;
     }
 }
 
@@ -17,3 +22,35 @@ function incrementScore() {
     money++;
     scoreDisplay.innerHTML = "$ " + money;
 }
+
+function setHealth(value){
+    /* Ensure the healthbar does not exceed 100 or go below 0*/
+    if (healthValue > 100){
+        healthValue = 100;
+    } else{
+        if (healthValue < 0){
+            healthValue = 0;
+        } else{
+            healthValue += value;
+        }
+    }
+    document.getElementById("clean-bar").style.width = healthValue + "%"
+}
+
+function handClickEventHandler(){
+    incrementScore();
+    setHealth(clickGainHealthRate);
+}
+
+/* All the functions to be executed when the page is run.*/
+function gameStart(){
+    /* Decrease health bar at a constant rate*/
+    let timerRate = 2;
+    setInterval(function(){
+        setHealth(-healthDecreaseRate);
+    }, timerRate);
+
+}
+
+
+gameStart();
