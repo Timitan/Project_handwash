14290428f0134551c6318c3478b69e0bb386e381
Gaurav kochar <Gauravkochar87@yahoo.com>
2020-04-01 14:12:05 -0700

    click counter seperate js

--- script/gameplay.js
+++ script/gameplay.js
@@ -186,24 +186,5 @@ function buyAbility(ability){
 
 }
 
-
-// ####################################################################
-// Functions to get number of clicks
-// ####################################################################
-function getClicks() {
-    //firebase.auth().onAuthStateChanged(user => {
-            //if (user) {
-                db.collection("users/").doc("zjZa12cXlFWCySmqqzF9N8LLt542").onSnapshot(
-                    function (snap) { //snap is just a name for what you captured
-                        //do something
-                        console.log(snap.data());
-                        document.getElementById("clickCount").innerHTML = snap.data().clicks;
-                    }
-                )
-            }
-      //  }
-    //}
-
-    getClicks();
     
     gameStart();
