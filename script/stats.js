d2f2601bc9831bedb0c16738b677ebc8ada984f3
Timothy Tan <tim.tan5145@gmail.com>
2020-04-01 14:18:19 -0700

    Read clicks from database for statistics page

--- script/stats.js
+++ script/stats.js
@@ -5,17 +5,23 @@
 // Functions to get number of clicks
 // ####################################################################
 function getClicks() {
-    //firebase.auth().onAuthStateChanged(user => {
-            //if (user) {
-                db.collection("users/").doc("zjZa12cXlFWCySmqqzF9N8LLt542").onSnapshot(
-                    function (snap) { //snap is just a name for what you captured
-                        //do something
-                        console.log(snap.data());
-                        document.getElementById("clickCount").innerHTML = snap.data().clicks;
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            
+            db.collection("users/").doc(user.uid)
+                .get().then(function (doc) {
+                    if (doc.exists) {
+                        console.log(doc.data());
+                        document.getElementById("clickCount").innerHTML = doc.data().clicks;
+                    } else {
+                        // doc.data() will be undefined in this case
+                        console.log("No such document!");
                     }
-                )
-            }
-      //  }
-    //}
+                }).catch(function (error) {
+                    console.log("Error getting document:", error);
+                });
+        }
+    });
+}
 
     getClicks();
