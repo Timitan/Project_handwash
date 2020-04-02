58e99d2a74304650a1bb4bdcc6b80dd8f9e834fd
Timothy Tan <tim.tan5145@gmail.com>
2020-04-02 09:41:55 -0700

    Leaderboard typo fix

--- script/leaderboard.js
+++ script/leaderboard.js
@@ -1,15 +1,15 @@
-// function updateLeaderBoard() {
-//     // Clear current scores.
-//     document.getElementById("leaderboard").innerHTML = "<tr><th>Name</th><th>Score</th></tr>";
+function updateLeaderBoard() {
+    // Clear current scores.
+    document.getElementById("leaderboard").innerHTML = "<tr><th>Name</th><th>Score</th></tr>";
 
-//     // Get the top 5 scores.
-//     db.collection("users").orderBy("clicks", "desc").limit(5).get().then((snapshot) => {
-//         snapshot.forEach((doc) => {
-//             document.getElementById("leaderboard").innerHTML += "<tr>" +
-//                 "<td>" + doc.data().name + "</td>" +
-//                 "<td>" + doc.data().clicks + "</td>" +
-//                 "</tr>";
-//         })
-//     })
-// }
-// window.onload = updateLeaderBoard;
+    // Get the top 5 scores.
+    db.collection("users").orderBy("clicks", "desc").limit(5).get().then((snapshot) => {
+        snapshot.forEach((doc) => {
+            document.getElementById("leaderboard").innerHTML += "<tr>" +
+                "<td>" + doc.data().name + "</td>" +
+                "<td>" + doc.data().clicks + "</td>" +
+                "</tr>";
+        })
+    })
+}
+window.onload = updateLeaderBoard;
