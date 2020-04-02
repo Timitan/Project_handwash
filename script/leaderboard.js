b98f6636535845b012fa927f470da9c8abe51716
Gaurav kochar <Gauravkochar87@yahoo.com>
2020-04-02 10:06:38 -0700

    updated leaderboard

--- script/leaderboard.js
+++ script/leaderboard.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 
@@ -6,28 +5,16 @@ function updateLeaderBoard() {
     // Clear current scores.
     document.getElementById("leaderboard").innerHTML = "<tr><th>Rank</th><th>Name</th><th>Health</th><th style='color:blue;font-size:large;'>Score</th></tr>";
     let i =1;
-=======
-function updateLeaderBoard() {
-    // Clear current scores.
-    document.getElementById("leaderboard").innerHTML = "<tr><th>Name</th><th>Score</th></tr>";
-
->>>>>>> 58e99d2a74304650a1bb4bdcc6b80dd8f9e834fd
     // Get the top 5 scores.
     db.collection("users").orderBy("clicks", "desc").limit(5).get().then((snapshot) => {
         snapshot.forEach((doc) => {
             document.getElementById("leaderboard").innerHTML += "<tr>" +
-<<<<<<< HEAD
                "<td>" + "#" + i + "</td>"+
                 "<td>" + doc.data().name + "</td>" +
                 "<td>" + doc.data().health + "</td>"+
                 "<td>" + doc.data().clicks + "</td>" 
                 "</tr>";
                 i++;
-=======
-                "<td>" + doc.data().name + "</td>" +
-                "<td>" + doc.data().clicks + "</td>" +
-                "</tr>";
->>>>>>> 58e99d2a74304650a1bb4bdcc6b80dd8f9e834fd
         })
     })
 }
